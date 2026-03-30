6157b0e6105b189af3b3ce47e7f9816a3f3c32fa
Teboflo <141273312+Teboflo@users.noreply.github.com>
2026-03-30 17:20:01 -0500

    Add files via upload

---
+++ script/porta.js
@@ -0,0 +1,86 @@
+const title = document.querySelector('.title');
+const botones = document.querySelector('.social-botones');
+const descripcion = document.querySelector('.descrip');
+const imagen = document.querySelector('.right-hero');
+
+window.addEventListener('scroll', () => {
+    if (window.innerWidth > 768) {
+        let scroll = window.scrollY;
+        let move = scroll * -0.3;
+
+        title.style.transform = `translateY(${move}px)`;
+        botones.style.transform = `translateY(${move}px)`;
+        descripcion.style.transform = `translateY(${move}px)`;
+        imagen.style.transform = `translateY(${move}px)`;
+    }
+});
+
+const secAbout = document.querySelector('.about-section');
+const aboTitle = document.querySelector('.title-about');
+const aboText = document.querySelector('.text-about');
+
+window.addEventListener('scroll', () => {
+    if (window.innerWidth > 768) {
+        let topabo = secAbout.offsetTop;
+        let scrollRelative = window.scrollY;
+        if (scrollRelative >= topabo) {
+            let scrollActual = scrollRelative - topabo;
+            let moves = scrollActual * -0.3;
+            aboTitle.style.transform = `translateY(${moves}px)`;
+            aboText.style.transform = `translateY(${moves}px)`;
+        }
+    }
+});
+
+
+
+const trabajos = [
+    { id: 'webpage', titulo: 'Web', subtitle: 'Page', imagen: '../image/porta/web.png', categoria: { nombre: 'Web Design', id: 'web' }, link: 'https://teboflo.github.io/one-connetion/', texto: 'Diseño web estructurado con enfoque en experiencia de usuario y jerarquía visual', cta: 'Visita el Sitio' },
+    { id: 'landing', titulo: 'Landing', subtitle: 'Page', imagen: '../image/porta/land.png', categoria: { nombre: 'Web Design', id: 'web' }, link: 'https://voxel-tebo.netlify.app/', texto: 'Landing page enfocada en conversión con estructura simple y mensajes directos', cta: 'Visita el Sitio' },
+    { id: 'wedding', titulo: 'Wedding', subtitle: 'Invitation', imagen: '../image/porta/wedd.png', categoria: { nombre: 'Web Design', id: 'web' }, link: 'https://teboflor.github.io/boda-ale-dany/', texto: 'Experiencia digital elegante y funcional para eventos especiales', cta: 'Visita el Sitio' },
+    { id: 'posters', titulo: 'Instagram', subtitle: 'Post', imagen: '../image/porta/inst.png', categoria: { nombre: 'Insta Post', id: 'post' }, link: 'https://www.instagram.com/growit.performance', texto: 'Post que combinan diseño estratégico y enfoque visual para aumentar retención, interacción y reconocimiento de marca', cta: 'Ver Posts' },
+    { id: 'prosanita', titulo: 'Prosanita', subtitle: 'Brand-kit', imagen: '../image/porta/pro.png', categoria: { nombre: 'Brand Kit', id: 'brand' }, link: 'https://www.instagram.com/pro.sanita', texto: 'Estilo de Marca Hand-Lettering minimalista & moderno, diseño y arquitectura de marca de manera profesional ', cta: 'Ir a Perfil' },
+    { id: 'growit', titulo: 'GrowiT', subtitle: 'Brand-kit', imagen: '../image/porta/gro.png', categoria: { nombre: 'Brand Kit', id: 'brand' }, link: 'https://www.instagram.com/growit.performance', texto: 'Marca Minimalista estilo Isologo moderno, construcción de marca en base a la visión de crecimiento digital', cta: 'Ir a Perfil' },
+    { id: 'one', titulo: 'One Connection', subtitle: 'Brand-kit', imagen: '../image/porta/one.png', categoria: { nombre: 'Brand Kit', id: 'brand' }, link: 'notfound.html', texto: 'Modelo de Marca Sencillo estilo minimalista isotipo representativo de valores y misión estratégica de la empresa', cta: 'No Disponible' }
+];
+
+const proyectosContent = document.querySelector('#contenedor-proyectos');
+const indice = document.querySelectorAll('.indice');
+
+function cargarTrabajos(proyectosElegidos) {
+    proyectosContent.innerHTML = '';
+
+    proyectosElegidos.forEach(trabajos => {
+        const div = document.createElement('div');
+        div.classList.add('proyecto');
+        div.innerHTML = `<div class="proyect-left">
+                        <img src="${trabajos.imagen}" alt="${trabajos.titulo}" class="proyect-img">
+                    </div>
+                    <div class="proyect-right">
+                        <h3 class="title">
+                            <u>${trabajos.titulo}</u> ${trabajos.subtitle}
+                        </h3>
+                        <p class="proyect-text">
+                            ${trabajos.texto}
+                        </p>
+                        <a href="${trabajos.link}" class="proyect-cta" target="_blank">${trabajos.cta}</a>
+                    </div>`;
+
+
+        proyectosContent.append(div);
+    });
+}
+
+cargarTrabajos(trabajos);
+
+indice.forEach(boton => {
+    boton.addEventListener('click', (e) => {
+        if (e.currentTarget.id != 'todos') {
+            const botonElegido = trabajos.filter(proyecto => proyecto.categoria.id === e.currentTarget.id);
+            cargarTrabajos(botonElegido);
+        } else {
+            cargarTrabajos(botonElegido);
+        }
+    });
+});
+
